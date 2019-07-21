21c5a26d65fcd988f2f659127e5c0d4164f2f85f
Eric Majerus <ackmanx@users.noreply.github.com>
2019-07-20 22:02:48 -0500

    added working pinyin converter. not saving yet.

--- apps/pinyin/client/app.js
+++ apps/pinyin/client/app.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import './app.css'
+import React, {useEffect, useRef} from 'react'
+import {convertToneNumbersToAccents} from './utils/convert-accents'
 
 export const App = () => {
+
+    const textareaEl = useRef(null)
+
+    useEffect(() => {
+        if (textareaEl.current) {
+            textareaEl.current.focus()
+        }
+    }, [])
+
+    const handleKeyUp = e => {
+        const textarea = e.target
+        textarea.value = convertToneNumbersToAccents(textarea.value)
+    }
+
     return (
-        <h1>hello pinyin</h1>
+        <textarea ref={textareaEl} onKeyUp={handleKeyUp}/>
     )
 }
--- apps/pinyin/server/routes/router.js
+++ apps/pinyin/server/routes/router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-    res.render('index', {title: 'Type Pinyin', appName: 'pinyin'})
+    res.render('index', {title: '干净 Type Pinyin', appName: 'pinyin'})
 })
 
 module.exports = router
