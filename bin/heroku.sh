6a37a9a9d97a93e146675cb54799e2a532874160
Eric Majerus <ackmanx@users.noreply.github.com>
2020-09-20 14:24:53 -0500

    heroku build comments

--- bin/heroku.sh
+++ bin/heroku.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Pushing to Heroku git remote will trigger a build and their dyno will automatically run yarn script `heroku-postbuild`
+# That will run this file, which will build the FE bundles
+
 # Setting yarn production to false will have it install devDependencies when Heroku builds the app, but keep NODE_ENV to production
 # We need these dependencies so webpack is downloaded onto Heroku servers and then it can be used to run the builds
 
